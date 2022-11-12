6c958f81b701ca87327e65a3aab3b6f6bbab3fe6
leo <e23882@hotmail.com>
2022-11-12 15:43:21 +0800

    [init] first version commit

---
+++ Model/Budget.cs
@@ -0,0 +1,16 @@
+﻿namespace BudgetService
+{
+    public abstract class Budget
+    {
+        #region Property
+        /// <summary>
+        /// 年月 yyyyMM
+        /// </summary>
+        public string YearMonth { get; set; }
+        /// <summary>
+        /// 金額(不含負數)
+        /// </summary>
+        public int Amount { get; set; }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetService
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
