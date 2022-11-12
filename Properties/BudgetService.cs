d8d932a948f83c001bd2677efad2b198f85f1c00
leo <e23882@hotmail.com>
2022-11-12 16:35:19 +0800

    [Add]加入沒有跨月的的case

--- Properties/BudgetService.cs
+++ Properties/BudgetService.cs
@@ -8,8 +8,11 @@ namespace BudgetService
 {
     public class BudgetService
     {
+        #region Declarations
         private readonly IBudgetRepo repo;
+        #endregion
 
+        #region MemberFunction
         public BudgetService(IBudgetRepo repo)
         {
             this.repo = repo;
@@ -26,14 +29,22 @@ namespace BudgetService
                 int.Parse(endDate.Substring(0, 4)),
                 int.Parse(endDate.Substring(4, 2)),
                 int.Parse(endDate.Substring(6, 2)));
-            if(endDateTime > startDateTime) 
+            if (endDateTime < startDateTime)
             {
                 return 0;
             }
 
+            if(startDateTime.Year == endDateTime.Year && startDateTime.Month == endDateTime.Month)
+            {
+                int days = (endDateTime - startDateTime).Days + 1;
+                int monthDays = DateTime.DaysInMonth(startDateTime.Year, startDateTime.Month);
+                int amount = repo.GetAll().Where(x => x.YearMonth == startDateTime.ToString("yyyyMM")).FirstOrDefault().Amount;
+                return amount / monthDays * days;
+            }
 
             return result;
             //repo.GetAll().Where(x=>x.YearMonth )
         }
+        #endregion
     }
 }
