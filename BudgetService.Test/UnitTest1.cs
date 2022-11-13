6ad56c834fcc5183b96b6de0ad85bd4b8040f83e
leo <e23882@hotmail.com>
2022-11-13 11:23:02 +0800

    [Modify]reformat code

--- BudgetService.Test/UnitTest1.cs
+++ BudgetService.Test/UnitTest1.cs
@@ -9,11 +9,14 @@ namespace BudgetService.Test
     public class Tests
     {
         #region Declarations
-        Service.BudgetService _service = null;
-        private IBudgetRepo _repo = null;
+
+        Service.BudgetService _service;
+        private IBudgetRepo _repo;
+
         #endregion
 
         #region MemberFunction
+
         [SetUp]
         public void Setup()
         {
@@ -24,14 +27,15 @@ namespace BudgetService.Test
         [TestCase]
         public void TestGetBudgetError()
         {
-            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>() {
-                new Budget(){YearMonth = "202205", Amount = 31},
-                new Budget(){YearMonth = "202204", Amount = 30},
-                new Budget(){YearMonth = "202202", Amount = 28},
-                new Budget(){YearMonth = "202201", Amount = 31},
+            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>()
+            {
+                new Budget() {YearMonth = "202205", Amount = 31},
+                new Budget() {YearMonth = "202204", Amount = 30},
+                new Budget() {YearMonth = "202202", Amount = 28},
+                new Budget() {YearMonth = "202201", Amount = 31},
             });
             decimal amount = _service.Query(
-                new DateTime(2022, 5, 1), 
+                new DateTime(2022, 5, 1),
                 new DateTime(2022, 1, 1));
             Assert.Zero(amount);
         }
@@ -39,42 +43,47 @@ namespace BudgetService.Test
         [TestCase]
         public void TestGetBudgetSignalMonth()
         {
-            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>() {
-                new Budget(){YearMonth = "202205", Amount = 31},
-                new Budget(){YearMonth = "202204", Amount = 30},
-                new Budget(){YearMonth = "202202", Amount = 28},
-                new Budget(){YearMonth = "202201", Amount = 31},
+            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>()
+            {
+                new Budget() {YearMonth = "202205", Amount = 31},
+                new Budget() {YearMonth = "202204", Amount = 30},
+                new Budget() {YearMonth = "202202", Amount = 28},
+                new Budget() {YearMonth = "202201", Amount = 31},
             });
             decimal amount = _service.Query(
-                new DateTime(2022, 3, 1), 
+                new DateTime(2022, 3, 1),
                 new DateTime(2022, 3, 18));
             Assert.Zero(amount);
         }
 
         [TestCase]
-        public void TestGetBudgetSameMonth() {
-            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>() {
-                new Budget(){YearMonth = "202205", Amount = 31},
-                new Budget(){YearMonth = "202204", Amount = 30},
-                new Budget(){YearMonth = "202202", Amount = 28},
-                new Budget(){YearMonth = "202201", Amount = 31},
+        public void TestGetBudgetSameMonth()
+        {
+            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>()
+            {
+                new Budget() {YearMonth = "202205", Amount = 31},
+                new Budget() {YearMonth = "202204", Amount = 30},
+                new Budget() {YearMonth = "202202", Amount = 28},
+                new Budget() {YearMonth = "202201", Amount = 31},
             });
             decimal amount = _service.Query(
-                new DateTime(2022, 1, 1), 
+                new DateTime(2022, 1, 1),
                 new DateTime(2022, 1, 31));
             Assert.AreEqual(amount, 31);
-
         }
+
         [TestCase]
-        public void TestGetBudgetCrossMonth() {
-            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>() {
-                new Budget(){YearMonth = "202205", Amount = 31},
-                new Budget(){YearMonth = "202204", Amount = 30},
-                new Budget(){YearMonth = "202202", Amount = 28},
-                new Budget(){YearMonth = "202201", Amount = 31},
+        public void TestGetBudgetCrossMonth()
+        {
+            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>()
+            {
+                new Budget() {YearMonth = "202205", Amount = 31},
+                new Budget() {YearMonth = "202204", Amount = 30},
+                new Budget() {YearMonth = "202202", Amount = 28},
+                new Budget() {YearMonth = "202201", Amount = 31},
             });
             decimal amount = _service.Query(
-                new DateTime(2022, 1, 1), 
+                new DateTime(2022, 1, 1),
                 new DateTime(2022, 2, 28));
             Assert.AreEqual(amount, 59);
         }
@@ -82,14 +91,15 @@ namespace BudgetService.Test
         [TestCase]
         public void TestGetBudgetCrossMultiMonth()
         {
-            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>() {
-                new Budget(){YearMonth = "202205", Amount = 31},
-                new Budget(){YearMonth = "202204", Amount = 30},
-                new Budget(){YearMonth = "202202", Amount = 28},
-                new Budget(){YearMonth = "202201", Amount = 31},
+            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>()
+            {
+                new Budget() {YearMonth = "202205", Amount = 31},
+                new Budget() {YearMonth = "202204", Amount = 30},
+                new Budget() {YearMonth = "202202", Amount = 28},
+                new Budget() {YearMonth = "202201", Amount = 31},
             });
             decimal amount = _service.Query(
-                new DateTime(2022, 2, 1), 
+                new DateTime(2022, 2, 1),
                 new DateTime(2022, 4, 30));
             Assert.AreEqual(amount, 58);
         }
@@ -97,17 +107,19 @@ namespace BudgetService.Test
         [Test]
         public void TestGetBudgetCrossMultiMonth_test()
         {
-            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>() {
-                new Budget(){YearMonth = "202203", Amount = 31},
-                new Budget(){YearMonth = "202204", Amount = 30},
-                new Budget(){YearMonth = "202202", Amount = 28},
-                new Budget(){YearMonth = "202201", Amount = 31},
+            _repo.GetAll().Returns(new System.Collections.Generic.List<Budget>()
+            {
+                new Budget() {YearMonth = "202203", Amount = 31},
+                new Budget() {YearMonth = "202204", Amount = 30},
+                new Budget() {YearMonth = "202202", Amount = 28},
+                new Budget() {YearMonth = "202201", Amount = 31},
             });
             decimal amount = _service.Query(
-                new DateTime(2022, 2, 28), 
+                new DateTime(2022, 2, 28),
                 new DateTime(2022, 4, 10));
-            Assert.AreEqual(amount, 1+31+10);
+            Assert.AreEqual(amount, 1 + 31 + 10);
         }
+
         #endregion
     }
 }
